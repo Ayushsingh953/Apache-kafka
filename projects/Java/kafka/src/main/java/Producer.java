21514f8f42e19c7d0c6f7a357652e553d5e4bb5e
ayushsingh953 <ayushsingh35323@gmail.com>
2024-07-02 16:33:30 +0530

    created new producer class

---
+++ projects/Java/kafka/src/main/java/Producer.java
@@ -0,0 +1,22 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class Producer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers","172.25.238.254:9092, 172.25.238.254:9093, 172.25.238.254:9094");
+        props.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String,String> producer = new KafkaProducer<>(props);
+
+        int numRecords = 100;
+        for (int i = 0; i < numRecords; i++) {
+            producer.send(new ProducerRecord<>("numbers",Integer.toString(i),Integer.toString(i)));
+        }
+
+        producer.close();
+    }
+}
